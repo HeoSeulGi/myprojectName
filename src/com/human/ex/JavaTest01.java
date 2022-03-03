509861936bacddf9c5a41f4deac6957710692a4f
HAN <HAN@DESKTOP-SKJD5BC>
2022-03-03 14:26:18 +0900

    first commit

---
+++ src/com/human/ex/JavaTest01.java
@@ -0,0 +1,15 @@
+package com.human.ex;
+
+public class JavaTest01 {
+    /*
+     * 프로그램의 시작.
+     * 단축키 설명.
+     */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        System.out.println("hi");
+        System.out.println("hi");
+        System.out.println("hi");
+	}
+
+}
---
+++ src/com/human/ex/JavaTest02.java
@@ -0,0 +1,12 @@
+package com.human.ex;
+
+public class JavaTest02 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Çã½½±â");
+		System.out.println("ald226@naver.com");
+		System.out.println("010-9440-8335");
+	}
+
+}
