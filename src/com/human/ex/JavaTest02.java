7e6acbb15fe71c95824f97396b35997974614d78
HAN <HAN@DESKTOP-SKJD5BC>
2022-03-03 14:40:25 +0900

    first

--- src/com/human/ex/JavaTest02.java
+++ src/com/human/ex/JavaTest02.java
@@ -4,9 +4,36 @@ public class JavaTest02 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Çã½½±â");
-		System.out.println("ald226@naver.com");
-		System.out.println("010-9440-8335");
+		System.out.print("*");
+		System.out.print("*");
+		System.out.print("*");
+		System.out.print("*");
+		System.out.print("*");
+		System.out.println("");
+		System.out.print("*");
+		System.out.print("*");
+		System.out.print("*");
+		System.out.print("*");
+		System.out.print("*");
+		System.out.println("");
+		System.out.print("*");
+		System.out.print("*");
+		System.out.print("*");
+		System.out.print("*");
+		System.out.print("*");
+		System.out.println("");
+		System.out.print("*");
+		System.out.print("*");
+		System.out.print("*");
+		System.out.print("*");
+		System.out.print("*");
+		System.out.println("");
+		System.out.print("*");
+		System.out.print("*");
+		System.out.print("*");
+		System.out.print("*");
+		System.out.print("*");
+		System.out.println("");
 	}
 
 }
